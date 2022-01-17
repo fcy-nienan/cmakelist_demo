9bec60bdde944d5cfcb15203dfa9c3e354956c41
nienan <nienan@atyun.net>
2022-01-17 12:01:23 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# 自己用来实验cmake命令的项目
+# 执行 在build目录下执行命令
+```
+/Applications/CLion.app/Contents/bin/cmake/mac/bin/cmake ..
+```
+将上面的cmake路径替换成自己cmake所在的路径
---
+++ build/CMakeFiles/cmakelist_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmakelist_demo.dir/main.cpp.o"
+  "cmakelist_demo"
+  "cmakelist_demo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cmakelist_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
